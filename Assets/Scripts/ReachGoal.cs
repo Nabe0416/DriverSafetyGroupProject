3adc29a55f906a92beeab8661205972f85e64335
der_nabe <34216014+Nabe0416@users.noreply.github.com>
2018-12-09 23:59:52 +0100

    New Feature: Car movement prototype

---
+++ Assets/Scripts/ReachGoal.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReachGoal : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print("Reached goal!");
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        print("Reached goal!");
+    }
+}
