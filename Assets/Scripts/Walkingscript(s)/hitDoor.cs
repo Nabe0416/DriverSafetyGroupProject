7ffa9bef0b61c857bbdbb3ad678be17ce857d843
maxvdv1 <vandervlugtmax@gmail.com>
2018-12-18 15:41:32 +0100

    movemnt + door and proto-(tutorial)scene
    movemnt + door and proto-(tutorial)scene

---
+++ Assets/Scripts/Walkingscript(s)/hitDoor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hitDoor : MonoBehaviour {
+ 
+    private void OnTriggerEnter(Collider other)
+    {
+        print("Opened door!");
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        print("Opened door!");
+    }
+}
