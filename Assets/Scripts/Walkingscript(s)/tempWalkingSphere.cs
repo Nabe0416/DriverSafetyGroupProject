05f1ac974539d8da46e7870d2db267b46f618725
maxvdv1 <vandervlugtmax@gmail.com>
2018-12-20 17:35:11 +0100

    walking Temporarly script
    walking tempscript

--- Assets/Scripts/Walkingscript(s)/tempWalkingSphere.cs
+++ Assets/Scripts/Walkingscript(s)/tempWalkingSphere.cs
@@ -4,13 +4,56 @@ using UnityEngine;
 
 public class tempWalkingSphere : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public bool usingVR = false;
+    public SteamVR_Action_Single tempWalkingVR; //Get the input of trigger(float) as the throttle.
+   
+    public SteamVR_Action_Vector2 rawTempWheelVR;
+ 
+
+    [Range(0.0f, 1000.0f)]
+    public float walkingSpeed = 100.0f;
+    [Range(0.0f, 100.0f)]
+    public float TurningCoefficient = 10.0f;
+
+    private float walkingValue;
+
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        UpdateInputValue();
+        Walking();
+      
+    }
+
+    void UpdateInputValue()
+    {
+        if (usingVR)
+        {
+            walkingValue = tempWalkingVR.GetAxis(SteamVR_Input_Sources.Touchpad);
+          
+        }
+        else
+        {
+            walkingValue = Input.GetAxis("Vertical");
+           
+            walkingValue = Input.GetAxis("Horizontal");
+        }
+    }
+
+    void Walking()
+    {
+        if (walkingValue != 0)
+        {
+            walkingSpeed = walkingValue * Time.deltaTime;
+        }
+
+    }
+
 }
+
